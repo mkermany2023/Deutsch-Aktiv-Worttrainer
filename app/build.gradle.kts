719c9933fe7cc4415fe5f6b81f60d042cb86dcde
mkermany2023 <138882122+mkermany2023@users.noreply.github.com>
2026-09-15 16:35:15 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.deutschaktiv.worttrainer"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.deutschaktiv.worttrainer"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
